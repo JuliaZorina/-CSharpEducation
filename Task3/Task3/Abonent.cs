2fe89b0024638c695f074c99d15cd9816d35eafb
Julia Zorina <yulia.a.zzo@gmail.com>
2024-08-12 21:44:51 +0400

    CRUD функциональность по работе с абонентом перенесена в класс Abonent

--- Task3/Task3/Abonent.cs
+++ Task3/Task3/Abonent.cs
@@ -45,5 +45,38 @@ namespace Task3
         }        
       }
     }
+
+    public static void CreateAbonent()//Должен добавлять в List<Abonent> нового абонента.
+    {
+      var abonent = new Abonent();
+      try
+      {
+        Console.WriteLine("Введите номер телефона");
+        abonent.PhoneNumber = Console.ReadLine();
+        Console.WriteLine("Введите имя абонента");
+        abonent.Name = Console.ReadLine();
+
+      }
+      catch
+      {
+        Console.WriteLine();
+      }
+
+    }
+
+    public static void ReadAbonent()//Должен находить абонента по номеру телефона или номер телефона по имени абонента.
+    {
+
+    }
+
+    public static void UpdateAbonent()//Должен обновлять данные абонента по номеру телефона или по имени абонента.
+    {
+
+    }
+
+    public static void DeleteAbonent()//Удалить всю информацию об абоненте.
+    {
+
+    }
   }
 }
--- Task3/Task3/Program.cs
+++ Task3/Task3/Program.cs
@@ -1,6 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
 
 namespace Task3
 {
@@ -8,42 +6,7 @@ namespace Task3
   {
     static void Main(string[] args)
     {
-      string path = @"..\..\phonebook.txt";
-      GetAbonents(GetStrings(path));
-    }
-
-    private static List<string> GetStrings(string path)
-    {
-      FileInfo file = new FileInfo(path);
-
-      if (!file.Exists)
-      {
-        file.Create();
-      }
-      var abonentsStrings = new List<string>();
-      using (StreamReader sr = new StreamReader(path))
-      {
-        string line;
-        while ((line = sr.ReadLine()) != null)
-        {
-          abonentsStrings.Add(line);
-        }
-      }
-      return abonentsStrings;
-    }
-    
-    private static List<Abonent> GetAbonents(List<string> abonentsStrings)
-    {
-      var abonents = new List<Abonent>();
-      foreach (var str in abonentsStrings)
-      {
-        var abonentInfo = str.Split(' ');
-        var abonent = new Abonent();
-        abonent.PhoneNumber = abonentInfo[0];
-        abonent.Name = abonentInfo[1];
-        abonents.Add(abonent);
-      }
-      return abonents;
-    }    
+      
+    }        
   }
 }
