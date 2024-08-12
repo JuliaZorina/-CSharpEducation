a93c16f7fa51b0a93882f4fe3edd4a0eb897a63f
Julia Zorina <yulia.a.zzo@gmail.com>
2024-08-12 20:20:55 +0400

    Создан метод для считывания строк из файла

--- Task3/Task3/Program.cs
+++ Task3/Task3/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace Task3
 {
@@ -6,6 +8,32 @@ namespace Task3
   {
     static void Main(string[] args)
     {
+      string path = @"..\..\phonebook.txt";
+      //GetStrings(path);
     }
+
+    private static void GetStrings(string path)
+    {
+      FileInfo file = new FileInfo(path);
+
+      if (file.Exists)
+      {
+        var abonentsStrings = new List<string>();
+        using (StreamReader sr = new StreamReader(path)) 
+        { 
+          string line;
+          while((line = sr.ReadLine()) != null)
+          {
+            abonentsStrings.Add(line);
+          }
+        }
+
+      }
+      else
+      {
+        file.Create();
+      }
+    }
+
   }
 }
