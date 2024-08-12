3863581960bcdf80791243c5cb6b3fb5cf423978
Julia Zorina <yulia.a.zzo@gmail.com>
2024-08-12 11:12:36 +0400

    Создан класс Abonent с полями "номер телефона" и "имя"

---
+++ Task3/Task3/Abonent.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace Task3
+{
+  class Abonent
+  {
+    private string phoneNumber;
+
+    public string PhoneNumber
+    {
+      get 
+      { 
+        return this.phoneNumber; 
+      }
+      set
+      {
+        if (string.IsNullOrEmpty(value))
+        {
+          throw new ArgumentException("Номер телефона не может быть пустой строкой!");
+        }
+        else
+        {
+          this.phoneNumber = value;
+        }
+      }
+    }
+
+    private string name;
+
+    public string Name
+    {
+      get
+      {
+        return this.name;
+      }
+      set
+      {
+        if (string.IsNullOrEmpty(value))
+        {
+          throw new ArgumentException("Имя не может быть пустой строкой!");
+        }
+        else
+        {
+          this.name = value;
+        }        
+      }
+    }
+  }
+}
