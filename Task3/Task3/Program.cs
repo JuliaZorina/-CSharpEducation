4a38efa0ab7a1dc1da69ffed25fb01b4b18b2f3c
Julia Zorina <yulia.a.zzo@gmail.com>
2024-08-12 20:30:25 +0400

    Создан метод для создания коллекции абонентов

--- Task3/Task3/Program.cs
+++ Task3/Task3/Program.cs
@@ -9,31 +9,41 @@ namespace Task3
     static void Main(string[] args)
     {
       string path = @"..\..\phonebook.txt";
-      //GetStrings(path);
+      GetAbonents(GetStrings(path));
     }
 
-    private static void GetStrings(string path)
+    private static List<string> GetStrings(string path)
     {
       FileInfo file = new FileInfo(path);
 
-      if (file.Exists)
-      {
-        var abonentsStrings = new List<string>();
-        using (StreamReader sr = new StreamReader(path)) 
-        { 
-          string line;
-          while((line = sr.ReadLine()) != null)
-          {
-            abonentsStrings.Add(line);
-          }
-        }
-
-      }
-      else
+      if (!file.Exists)
       {
         file.Create();
       }
+      var abonentsStrings = new List<string>();
+      using (StreamReader sr = new StreamReader(path))
+      {
+        string line;
+        while ((line = sr.ReadLine()) != null)
+        {
+          abonentsStrings.Add(line);
+        }
+      }
+      return abonentsStrings;
     }
-
+    
+    private static List<Abonent> GetAbonents(List<string> abonentsStrings)
+    {
+      var abonents = new List<Abonent>();
+      foreach (var str in abonentsStrings)
+      {
+        var abonentInfo = str.Split(' ');
+        var abonent = new Abonent();
+        abonent.PhoneNumber = abonentInfo[0];
+        abonent.Name = abonentInfo[1];
+        abonents.Add(abonent);
+      }
+      return abonents;
+    }    
   }
 }
