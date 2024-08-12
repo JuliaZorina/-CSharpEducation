d8922de6e1a3025c06586d068c2feb91dca71c26
Julia Zorina <yulia.a.zzo@gmail.com>
2024-08-12 14:53:41 +0400

    создан класс Phonebook, реализующий паттерн singletone

---
+++ Task3/Task3/Phonebook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task3
+{
+  class Phonebook
+  {
+    private static Phonebook instance;
+    private Phonebook() 
+    { }
+
+    public static Phonebook GetPhonebook()
+    {
+      if(instance==null)
+        instance = new Phonebook();
+      return instance;
+    }
+  }
+}
