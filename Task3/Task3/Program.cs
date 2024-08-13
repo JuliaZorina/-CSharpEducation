3a145f3acc8219f0830034d09fee9cbdf090d813
Julia Zorina <yulia.a.zzo@gmail.com>
2024-08-13 11:38:14 +0400

    Создано меню для работы с телефонной книгой, осуществлен метод выхода из программы

--- Task3/Task3/Program.cs
+++ Task3/Task3/Program.cs
@@ -9,6 +9,61 @@ namespace Task3
       const string path = @"..\..\phonebook.txt";
 
       Phonebook phonebook = Phonebook.getPhonebook(path);
-    }        
+
+      Menu();
+
+    }    
+    private static void Menu()
+    {
+      var abonent = new Abonent();
+
+      Console.WriteLine("Нажмите 1 для добавления нового контакта.\nНажмите 2 для просмотра уже существующего контакта." +
+        "\nНажмите 3 для редактирования уже существующего контакта.\nНажмите 4 для удаления контакта." +
+        "\nНажмите 5 для выхода из программы.");
+      
+      var menu = Console.ReadLine();
+      
+      switch (menu)
+      {
+        case "1":
+          abonent.CreateAbonent();
+          break;
+        case "2":
+          abonent.ReadAbonent();
+          break;
+        case "3":
+          abonent.UpdateAbonent();
+          break;
+        case "4":
+          abonent.DeleteAbonent();
+          break;
+        case "5":
+          Exit();
+          break;
+        default:
+          Console.WriteLine("Введено некорректное значение!");
+          Menu();
+          break;
+      }
+    }
+    
+    private static void Exit()
+    {
+      Console.WriteLine("Вы действительно хотите выйти из программы? [y/n]");
+      var exit = Console.ReadLine();
+      if (exit == "y")
+      {
+        Console.WriteLine("Выход из программы...");
+      }
+      else if(exit == "n")
+      {
+        Menu();
+      }
+      else
+      {
+        Console.WriteLine("Введено некорректное значение!");
+        Exit();
+      }
+    }
   }
 }
